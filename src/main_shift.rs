b890caa2e119f0734218c938cb69d9a736f49204
wentong liu <64199482+wentong-wow@users.noreply.github.com>
2025-02-20 03:56:48 +0000

    test

---
+++ src/main_shift.rs
@@ -0,0 +1,20 @@
+// shift line
+fn foo(n: i32) -> i32 {
+    n + 1
+}
+
+fn main() {
+    // Absolute path lint issue: use an absolute path for standard library reference
+    let _v: ::std::vec::Vec<i32> = vec![1, 2, 3];
+
+    // Absurd extreme comparison lint issue: comparing a value with an extreme that will always be true or false
+    let x = 10;
+    if x < i32::MIN {
+        println!("This will never print, but the lint will be triggered.");
+    }
+
+    let _ = foo(x);
+
+    // Almost complete range lint issue: creating a range that is very close to being complete
+    let _ = 'a'..'z';
+}
